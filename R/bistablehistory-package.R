a9510903c148e6dabb8191d83ae850f014d430cc
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2020-09-29 11:06:22 +0200

    Project skeleton

---
+++ R/bistablehistory-package.R
@@ -0,0 +1,16 @@
+#' The 'bistablehistory' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name bistablehistory-package
+#' @aliases bistablehistory
+#' @useDynLib bistablehistory, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.19.3. https://mc-stan.org
+#'
+NULL
