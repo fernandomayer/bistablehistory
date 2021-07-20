13b416bea89f5386bf3f94ac3005ebce4c509af9
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2021-07-20 22:55:20 +0200

    predict()

---
+++ R/predict.R
@@ -0,0 +1,44 @@
+#' Computes predicted dominance phase durations using posterior predictive distribution.
+#'
+#' Computes predicted dominance phase durations using fitted model.
+#'
+#' @param object An object of class [cumhist][cumhist-class()]
+#' @param summary Whether summary statistics should be returned instead of
+#' raw sample values. Defaults to \code{TRUE}
+#' @param probs The percentiles used to compute summary, defaults to NULL (no CI).
+#' @param ... Unused
+#'
+#' @return If \code{summary=FALSE}, a numeric matrix iterationsN x clearN.
+#' If \code{summary=TRUE} but \code{probs=NULL} a vector of mean predicted durations.
+#' If \code{summary=TRUE} and \code{probs} is not \code{NULL}, a data.frame
+#' with a column _"Predicted"_ (mean) and a column for each specified quantile.
+#' @export
+#'
+#' @seealso \code{\link{fit_cumhist}}
+#' @examples
+#' \donttest{
+#' br_fit <- fit_cumhist(br_singleblock, state="State", duration="Duration")
+#' predicted <- predict(br_fit)
+#'
+#' # full posterior prediction samples
+#' predictions_samples <- predict(br_fit, summary=FALSE)
+#' }
+predict.cumhist <-  function(object, summary=TRUE, probs=NULL, ...) {
+  # extracting parameters
+  lm_params <- rstan::extract(object$fit, pars="lm_param")$lm_param
+
+  if (object$family == "gamma") {
+    predictions <- lm_params[, 1, ] * lm_params[, 2, ]
+  }
+
+  # raw samples
+  if (!summary) return(predictions)
+
+  # means
+  predictions_mean <- apply(as.matrix(predictions), MARGIN=2, FUN=mean)
+  if (is.null(probs)) return(predictions_mean)
+
+  # full summary
+  tibble::tibble(Predicted = predictions_mean) %>%
+    dplyr::bind_cols(tibble::as_tibble(t(apply(as.matrix(predictions), MARGIN=2, FUN=quantile, probs=probs))))
+}
