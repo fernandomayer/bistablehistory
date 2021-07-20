9b7f05c2128c1d6a65f736be36520de74ecf6499
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2021-07-20 21:47:56 +0200

    LOO & WAIC

---
+++ R/loo.R
@@ -0,0 +1,17 @@
+#' Computes an efficient approximate leave-one-out
+#' cross-validation via loo library. It can be used
+#' for a model comparison via loo::loo_compare() function.
+#'
+#' @param x A [cumhist][cumhist-class()] object
+#' @param ... unused
+#'
+#' @return A named list, see [loo::loo()] for details.
+#' @export
+#'
+#' @examples
+#' data(br_singleblock)
+#' gamma_fit <- fit_cumhist(br_singleblock, state="State", duration="Duration")
+#' loo_gamma <- loo(gamma_fit)
+loo.cumhist <- function(x, ...) {
+  rstan::loo(x$fit, cores = future::availableCores())
+}
---
+++ R/waic.R
@@ -0,0 +1,23 @@
+#' Computes widely applicable information criterion
+#' (WAIC).
+#'
+#' @description Computes widely applicable information criterion
+#' via [loo][loo-package] library. It can be used for a model comparison via
+#' [loo::loo_compare()][loo::loo_compare] function.
+#'
+#' @param x A [cumhist][cumhist-class()] object.
+#'
+#' @return A named list, see [loo::waic()] for details.
+#' @export
+#'
+#' @examples
+#' data(br_singleblock)
+#' gamma_fit <- fit_cumhist(br_singleblock, state="State", duration="Duration")
+#' waic_gamma <- waic(gamma_fit)
+#' lognormal_fit <- fit_cumhist(br_singleblock, state="State", duration="Duration", family="lognormal")
+#' waic_lognormal <- waic(lognormal_fit)
+#' loo::loo_compare(waic_gamma, waic_lognormal)
+waic.cumhist <- function(x, ...) {
+  log_lik <- loo::extract_log_lik(x$fit, "log_lik", merge_chains = FALSE)
+  loo::waic(log_lik)
+}
