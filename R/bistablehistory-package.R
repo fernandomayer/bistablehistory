4cdd723b0b346e4c68bffa52e0a7808bea2defff
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2021-07-22 12:46:58 +0200

    More imports

--- R/bistablehistory-package.R
+++ R/bistablehistory-package.R
@@ -9,10 +9,11 @@
 #' @import methods
 #' @import Rcpp
 #' @importFrom rstan sampling
-#' @importFrom loo loo waic
+#' @importFrom loo loo waic relative_eff psis E_loo
 #' @importFrom tibble tibble as_tibble
 #' @importFrom dplyr bind_cols row_number n mutate group_by ungroup
 #' @importFrom future availableCores
+#' @importFrom bayesboot rudirichlet
 #'
 #' @references
 #' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.21.2. https://mc-stan.org
