b28d7f0a9ca996d6e2b310c8bbc8b7fae34591fd
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2021-07-22 16:42:27 +0200

    import inv.logit

--- R/bistablehistory-package.R
+++ R/bistablehistory-package.R
@@ -14,6 +14,7 @@
 #' @importFrom dplyr bind_cols row_number n mutate group_by ungroup
 #' @importFrom future availableCores
 #' @importFrom bayesboot rudirichlet
+#' @importFrom boot inv.logit
 #'
 #' @references
 #' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.21.2. https://mc-stan.org
