9cd1d66bd73773925af2df3d4609baf2f1324bb0
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2021-07-23 14:59:32 +0200

    Added extract_history

--- R/compute_history.R
+++ R/compute_history.R
@@ -79,7 +79,41 @@ compute_history <- function(data,
   h <- fast_history_compute(data, tau, mixed_state, history_init)
 
   # adding state names
-  colnames(h) <- levels(factor(br_singleblock$State))[1:2]
+  colnames(h) <- levels(factor(data$State))[1:2]
 
   h
 }
+
+#' @export
+#' @keywords internal
+extract_history.cumhist <- function(object, ...)
+{
+  # data as table
+  data <- tibble::tibble(state = object$data$state,
+                         duration = object$data$duration,
+                         irandom= object$data$irandom,
+                         run_start = object$data$run_start,
+                         session_tmean = object$data$session_tmean)
+
+  # history parameters
+  tau <-  bistablehistory::check_fixed_history_parameter("tau",
+                                                         history_tau(object, probs=NULL)$Estimate,
+                                                         object$data$randomN, Inf)
+  mixed_state <- bistablehistory::check_fixed_history_parameter("mixed_state",
+                                                         history_mixed_state(object, probs=NULL)$Estimate,
+                                                         object$data$randomN, Inf)
+  history_init <- bistablehistory::evaluate_history_init(object$data$history_starting_values)
+
+  # computing the history
+  h <- fast_history_compute(data, tau, mixed_state, history_init)
+
+
+  # adding state names
+  colnames(h) <- levels(factor(object$data$original_state))[1:2]
+
+  h
+}
+
+#' @export
+#' @keywords internal
+extract_history <- function(object, ...) { UseMethod("extract_history") }
