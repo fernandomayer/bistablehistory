193ae55731bfed43406fe4803acfeb1cb5af0529
Alexander (Sasha) Pastukhov <alexander.pastukhov@uni-bamberg.de>
2021-09-23 16:21:08 +0200

    Single subject many runs data

--- R/data.R
+++ R/data.R
@@ -14,6 +14,22 @@
 #' @source  \doi{10.1167/11.10.12}
 "br_singleblock"
 
+#' Single experimental session for binocular rivalry stimulus
+#'
+#' A single subject / multiple runs dataset for binocular rivalry.
+#'
+#' @format A data frame with 76 rows and 6 variables:
+#' \describe{
+#'   \item{Observer}{Participant ID, all rows contain _"ap"_}
+#'   \item{Group}{Display, all rows contain _"BR"_}
+#'   \item{Block}{Run / block index}
+#'   \item{Time}{Time relative to the run onset in _seconds_}
+#'   \item{State}{Index of a perceptually dominant state, _1_, _2_ - clear perceptual state, _3_ mixed / transition phase}
+#'   \item{Duration}{Duration of a dominance phase in _seconds_. Note that the duration for the last dominance phase is curtailed and, therefore, set to zero.}
+#' }
+#' @source  \doi{10.1167/11.10.12}
+"br_single_subject"
+
 
 #' Multirun data for two participants, kinectic-depth effect display
 #'
